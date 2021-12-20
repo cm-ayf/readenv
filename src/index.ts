f4cb11e4c52ba6340042fb1cad39cdc50cd3c3bf
CallMe AsYouFeel <cm.ayf2734@gmail.com>
2021-12-20 20:36:09 +0900

    option.default allows any type

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,15 @@
  * @returns object with common keys with options and string result values
  * @throws if more than one key has neither environment variable nor default; throws everything at once
  */
-export default function readenv<T>(options: {
-    [K in keyof T]: K extends string ? Option : never;
+export default function readenv<D extends object>(options: {
+    [K in keyof D]: Option<D[K]>;
 }) {
-    const env = {} as { [K in keyof T]: string };
+    const env = {} as { [K in keyof D]: D[K] | string };
     const errs = [] as Error[];
 
     for (const key in options) {
         let value =
-            process.env[options[key]['from'] ?? key] ?? options[key]['default'];
+            process.env[options[key]['from'] ?? key] ?? options[key]['default']
         if (value !== undefined) env[key] = value;
         else errs.push(new Error(`${options[key]['from'] ?? key} not found`));
     }
@@ -24,12 +24,12 @@ export default function readenv<T>(options: {
 /**
  * specifies how to read variable. one `Option` per one variable.
  */
-interface Option {
+interface Option<D> {
     /**
      * default value used when variable was not found.
      * if omitted throws error if variable was not found.
      */
-    default?: string;
+    default?: D;
     /**
      * alternative variable name.
      * if omitted use property name of `options`.
